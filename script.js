448621c9bcbd604766011a10645cdf6d7b2c901b
andrewsilent <36265247+andrewsilent@users.noreply.github.com>
2021-01-15 16:41:16 +0200

    add basic classes

---
+++ script.js
@@ -0,0 +1,24 @@
+class User {
+  constructor(name, surname, year) {
+    this.name = name;
+    this.surname = surname;
+    this.year = year;
+
+  }
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+}
+
+class Student extends User {
+  constructor(name, surname, year) {
+    super(name, surname, year);
+  }
+  getCourse() {
+    return new Date().getFullYear() - this.year;
+  }
+}
+
+const student1 = new Student('Alex', 'Smith', 2019);
+const student2 = new Student('Emily', 'Blant', 2018);
+const student3 = new Student('Emily', 'Blant', 2021);
